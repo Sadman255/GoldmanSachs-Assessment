44cbbce71a4346447c9cf5af3a55f00bc9e5686e
SadmanAnwarConvo <sadman.anwar@convorelay.com>
2021-03-28 23:14:16 -0400

    done assessment problems

--- Assessment_Problems.js
+++ Assessment_Problems.js
@@ -13,6 +13,8 @@ function duplicate(array){
 
 //console.log(duplicate([1,2,3,4,5])) will return [1,2,3,4,5,1,2,3,4,5]
 
+
+
 // Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, 
 // "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
 
@@ -66,3 +68,20 @@ function fizzBuzz(){
     console.log(output);
   }
 }
+
+//What will be returned by each of these?
+// console.log("hello" || "world")
+
+// console.log("foo" && "bar")
+
+Output 1: hello
+Output 2:  bar  
+
+
+// Write an immediately invoked function expression
+var output = (function () {
+    var name = "Sadman";
+    return name;
+})();
+
+output; 
